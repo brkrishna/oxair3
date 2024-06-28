acd31130802a7f85334c5a6df4487f363a6689aa
Ramakrishna <brkrishna@gmail.com>
2024-06-28 12:05:02 +0000

    updated tel and email

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Define __filename and __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -7,15 +7,15 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images:{
-unoptimized:true,
+  output: "export",
+  images: {
+    unoptimized: true,
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
+    includePaths: [path.join(__dirname, "src/styles")],
   },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias["@"] = path.join(__dirname, "src");
     return config;
   },
 };
--- src/components/ContactUsToday.jsx
+++ src/components/ContactUsToday.jsx
@@ -1,36 +1,48 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Image from 'next/image';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Image from "next/image";
 //import {FaPhoneAlt, FaEnvelope} from 'react-icons'
-import PhoneIcon from "../assets/icons/PhoneIcon.png"
-import EmailIcon from "../assets/icons/EmailIcon.png"
+import PhoneIcon from "../assets/icons/PhoneIcon.png";
+import EmailIcon from "../assets/icons/EmailIcon.png";
 
 const ContactUsToday = () => {
-
-
   return (
     <>
-   <div className="contact-us-banner">
+      <div className="contact-us-banner">
         <Row className="align-items-center p-3">
           <Col md={6} className="text-white border-r">
             <h4>Contact Us Today</h4>
-            <p>Unleash the power of our oxygen generators to transform your operations.</p>
+            <p>
+              Unleash the power of our oxygen generators to transform your
+              operations.
+            </p>
           </Col>
           <Col md={6} className="text-center d-flex justify-content-around">
             <div className="contact-info">
               {/* <FaPhoneAlt className="icon" /> */}
-              <Image className="icon" src={PhoneIcon} alt='Contact' width={50} height={50} />
-              <span className="contact-text">044-29560328</span>
+              <Image
+                className="icon"
+                src={PhoneIcon}
+                alt="Contact"
+                width={50}
+                height={50}
+              />
+              <span className="contact-text">+91-44-29560328</span>
             </div>
             <div className="contact-info">
               {/* <FaEnvelope className="icon" /> */}
-              <Image className="icon" src={EmailIcon} alt='Email' width={50} height={50} />
-              <span className="contact-text">oxairindia@gmail.com</span>
+              <Image
+                className="icon"
+                src={EmailIcon}
+                alt="Email"
+                width={50}
+                height={50}
+              />
+              <span className="contact-text">inquiries@oxair.in</span>
             </div>
           </Col>
         </Row>
-    </div>
-      
+      </div>
     </>
   );
 };
