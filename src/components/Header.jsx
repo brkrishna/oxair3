9858bc989b60026c0406c38793d85a28f5b3a998
Venkatramaiah Usirikayala <venkatramaiahu@ispace.com>
2024-09-06 15:25:28 +0530

    banner-3 replaced with new banner and toggle menu designed as per the document

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,22 +17,22 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto flex-column flex-lg-row border-2">
-            <Nav.Link className="mx-2" href="/">Home</Nav.Link>
-            <Nav.Link className="mx-2" href="/aboutOxair">About Us</Nav.Link>
-            <NavDropdown className="mx-2 custom-dropdown" title="Our Products" id="collapsible-nav-dropdown">
+            <Nav.Link  href="/">Home</Nav.Link>
+            <Nav.Link  href="/aboutOxair">About Us</Nav.Link>
+            <NavDropdown className="custom-dropdown" title="Our Products" id="collapsible-nav-dropdown">
             <NavDropdown.Item className="products"href="../highPerformanceModels">High Performance Models</NavDropdown.Item>
               <NavDropdown.Item className="products" href="../superSaverModels">Super Saver Models</NavDropdown.Item>
               {/* <NavDropdown.Item className="products" href="/oxairPressureVessels">Oxair Pressure Vessels</NavDropdown.Item>
               <NavDropdown.Item className="products" href="/">Other Products</NavDropdown.Item> */}
             </NavDropdown>
-            <NavDropdown className="mx-2 custom-dropdown" title="Resources" id="collapsible-nav-dropdown-resources">
+            <NavDropdown className="custom-dropdown" title="Resources" id="collapsible-nav-dropdown-resources">
               {/* <NavDropdown.Item className="products" href="/understandingOurProducts">Understanding Our Products</NavDropdown.Item>
               <NavDropdown.Item className="products" href="/faqs">FAQs</NavDropdown.Item> */}
               <NavDropdown.Item className="products" href="/companyAndIndustryNews">Company and Industry News</NavDropdown.Item>
               <NavDropdown.Item className="products" href="/gallery">Gallery</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link className="mx-2" href="/contactUs">ContactUs</Nav.Link>
-            <Nav.Link className="mx-2" href="/getQuote">
+            <Nav.Link className="" href="/contactUs">ContactUs</Nav.Link>
+            <Nav.Link className="" href="/getQuote">
               <ButtonComponent className="get-quote" label="Get Quote" />
             </Nav.Link>
           </Nav>
