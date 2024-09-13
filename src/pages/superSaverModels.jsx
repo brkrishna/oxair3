58f23ec1c272c6e626157263b8f4d1f116a6e97e
Narasimha <narasimharao.konda@ispace.com>
2024-09-13 12:18:06 +0530

    Images optimization and SCO content changes and new titles and description added

--- src/pages/superSaverModels.jsx
+++ src/pages/superSaverModels.jsx
@@ -3,23 +3,20 @@ import InnerPageBanner from "@/components/InnerPageBanners";
 import SsModels from "@components/SuperSaveModel"
 import Head from "next/head";
 
-
-
 const productPage = () => {
   return (
     <>
     <Head>
-      <title>Oxair - Super Saver(SS) Oxygen Generator Models</title>
+      <title>Oxair - Super Saver Medical Oxygen Generators</title>
       <meta name="description" content="We provide super saver oxygen generators, including the OA 60 SS, OA 80 SS, OA 120 SS, OA 200 SS, OA 250 SS, OA 320 SS" />
       <link rel="icon" href="/icons/favicon-16x16.png" type="image/x-icon" />
+      <link rel="canonical" href="https://example.com/blog/original-post" key="canonical" />
     </Head>
       <InnerPageBanner
         heading="Our Products"
-        subHeading="Super Save Models"
+        subHeading="Super Saver Models"
       />
       <SsModels/>
-  
-      
     </>
   );
 };
